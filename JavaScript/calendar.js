884361a70e2a69dd7ee03145c9fc2b4d15b96369
Saul <82818499+SaulExpo@users.noreply.github.com>
2025-03-28 16:49:38 +0000

    Add files via upload

--- JavaScript/calendar.js
+++ JavaScript/calendar.js
@@ -1,82 +1,91 @@
 document.addEventListener('DOMContentLoaded', function() {
     getUserProfile().then(user => {
         console.log(user.email);
-        getRoutines(user.email).then(routines => {
-            var $rutineSelect = $('#eventRutine');
-            var $editEventRutineSelect = $('#editEventRutine');
-            routines.forEach(function(rutina) {
-                $rutineSelect.append('<option value="' + rutina.id + '">' + rutina.rutine.name + '</option>');
-                $editEventRutineSelect.append('<option value="' + rutina.id + '">' + rutina.rutine.name + '</option>');
+        getUserRoutines(user.email).then(userRoutines => {
+            getRoutines().then(routines => {
+                console.log(routines);
+                var $rutineSelect = $('#eventRutine');
+                var $editEventRutineSelect = $('#editEventRutine');
+                routines.forEach(function(rutina) {
+                    $rutineSelect.append('<option value="' + rutina.id + '">' + rutina.name + '</option>');
+                    $editEventRutineSelect.append('<option value="' + rutina.id + '">' + rutina.name + '</option>');
+                });
+                console.log(userRoutines)
 
-            });
-            var events = routines.map(function(rutina) {
-                return {
-                    title: rutina.rutine.name,
-                    start: rutina.date,
-                    extendedProps: {
-                        description: rutina.rutine.description
+                var events = userRoutines.map(function(rutina) {
+                    return {
+                        title: rutina.rutine.name,
+                        start: rutina.date,
+                        extendedProps: {
+                            description: rutina.rutine.description
+                        },
+                        id: rutina.id
+                    };
+                });
+                var calendarEl = document.getElementById('calendar');
+                var calendar = new FullCalendar.Calendar(calendarEl, {
+                    editable: true,
+                    droppable: true,
+                    headerToolbar: {
+                        left: 'prev,next today',
+                        center: 'title',
+                        right: 'dayGridMonth,dayGridWeek,dayGridDay,listDay'
                     },
-                    id: rutina.id
-                };
-            });
-            var calendarEl = document.getElementById('calendar');
-            var calendar = new FullCalendar.Calendar(calendarEl, {
-                editable: true,
-                droppable: true,
-                headerToolbar: {
-                    left: 'prev,next today',
-                    center: 'title',
-                    right: 'dayGridMonth,dayGridWeek,dayGridDay,listDay'
-                },
-                locale: 'es',
-                initialView: 'dayGridMonth',
-                aspectRatio: 1.5,
-                themeSystem: 'bootstrap5',
-                events: events, // Cargar eventos desde el JSON
-                dateClick: function(info) {
-                    $('#eventModal').modal('show');
-                    $('#eventStartDate').val(info.dateStr + 'T09:00'); // Establecer la fecha de inicio por defecto
-                },
-                eventClick: function(info) {
-                    selectedEvent = info.event;
-                    // Llenar el modal de edición con los datos del evento seleccionado
-                    $('#editEventDate').val(selectedEvent.start.toISOString().slice(0, 16)); // Formato para datetime-local
-                    $('#editEventRutine').val(selectedEvent.id); // Setear la rutina seleccionada
-                    $('#editEventModal').modal('show');
-                }
-            });
-            calendar.render();
+                    locale: 'es',
+                    initialView: 'dayGridMonth',
+                    aspectRatio: 1.5,
+                    themeSystem: 'bootstrap5',
+                    events: events, // Cargar eventos desde el JSON
+                    dateClick: function(info) {
+                        $('#eventModal').modal('show');
+                        $('#eventDate').val(info.dateStr + 'T09:00'); // Establecer la fecha de inicio por defecto
+                    },
+                    eventClick: function(info) {
+                        selectedEvent = info.event;
+                        console.log(selectedEvent.id)
+                        // Llenar el modal de edición con los datos del evento seleccionado
+                        $('#editEventDate').val(selectedEvent.start.toISOString().slice(0, 16)); // Formato para datetime-local
+                        $('#editEventRutine').val(selectedEvent.id); // Setear la rutina seleccionada
+                        $('#editEventModal').modal('show');
+                    },eventDrop: function(info) {
+                        var rutine= userRoutines.find(function(r) { return r.id == info.event.id; });
+                        editUserRoutine(user, rutine, info.event.start, rutine.documentId)
+                    }
+                });
+                calendar.render();
 
-            // Función para agregar un evento desde el formulario
-            $('#eventForm').on('submit', function(e) {
-                e.preventDefault();
+                // Función para agregar un evento desde el formulario
+                $('#eventForm').on('submit', function(e) {
+                    e.preventDefault();
 
-                var eventDate = $('#eventDate').val();
-                var rutineId = $('#eventRutine').val();
-                var rutine = routines.find(function(r) { return r.id == rutineId; });
-                console.log(rutine.rutine)
-                addUserRoutine(user, rutine.rutine, eventDate)
-            });
+                    var eventDate = $('#eventDate').val();
+                    var rutineId = $('#eventRutine').val();
+                    var rutine = routines.find(function(r) { return r.id == rutineId; });
+                    console.log(rutine)
+                    addUserRoutine(user, rutine, eventDate)
+                });
 
-            // Editar evento
-            $('#editEventForm').on('submit', function(e) {
-                e.preventDefault();
+                // Editar evento
+                $('#editEventForm').on('submit', function(e) {
+                    e.preventDefault();
 
-                var eventDate = $('#editEventDate').val();
-                var rutineIdNew = $('#editEventRutine').val();
-                var rutineId = selectedEvent.id
-                var rutineNew = routines.find(function(r) { return r.id == rutineId2; });
-                var rutine = routines.find(function(r) { return r.id == rutineId; });
+                    var eventDate = $('#editEventDate').val();
+                    var rutineIdNew = $('#editEventRutine').val();
+                    var rutineId = selectedEvent.id
+                    var rutineNew = userRoutines.find(function(r) { return r.id == rutineIdNew; });
+                    var rutine = userRoutines.find(function(r) { return r.id == rutineId; });
 
-                editUserRoutine(user, rutineNew, eventDate, rutine.documentId)
-            });
+                    editUserRoutine(user, rutineNew, eventDate, rutine.documentId)
+                });
 
-            // Eliminar evento
-            $('#deleteEventButton').on('click', function() {
-                var rutineId = selectedEvent.id
-                var rutine = routines.find(function(r) { return r.id == rutineId; })
-                deleteUserRoutine(rutine.documentId)
-            });
+                // Eliminar evento
+                $('#deleteEventButton').on('click', function() {
+                    var rutineId = selectedEvent.id
+                    var rutine = userRoutines.find(function(r) { return r.id == rutineId; })
+                    deleteUserRoutine(rutine.documentId)
+                });
+            })
+
         });
     })
 
---
+++ JavaScript/getUserRoutines.js
@@ -0,0 +1,145 @@
+async function getRoutines(){
+    const url = 'http://localhost:1337/api/rutines?populate=*';
+    try {
+        // Hacemos una solicitud GET a la API
+        const response = await fetch(url);
+
+        // Verificamos si la solicitud fue exitosa
+        if (!response.ok) {
+            throw new Error('No se pudo obtener los datos de los reviews');
+        }
+
+        // Parseamos la respuesta a JSON
+        const routines = await response.json()
+        // Filtramos los films para que solo contengan la categoría "Animation"
+
+        return routines.data;
+
+
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+
+async function getUserRoutines(email){
+    const url = 'http://localhost:1337/api/user-routines?populate=*';
+    try {
+        // Hacemos una solicitud GET a la API
+        const response = await fetch(url);
+
+        // Verificamos si la solicitud fue exitosa
+        if (!response.ok) {
+            throw new Error('No se pudo obtener los datos de los reviews');
+        }
+
+        // Parseamos la respuesta a JSON
+        const routines = await response.json()
+        // Filtramos los films para que solo contengan la categoría "Animation"
+        const userRoutines = routines.data.filter(routine =>
+            routine.user_app.email === email
+        );
+        return userRoutines;
+
+
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+async function addUserRoutine(user, rutine, date){
+
+
+    const formData = {
+        data: {
+            user_app: { connect: [user] }, // Relación con el usuario
+            rutine: { connect: [rutine] },
+            date: date
+        }
+    };
+
+    console.log(formData);
+
+    try {
+        // Enviar los datos a Strapi usando fetch (puedes usar axios también)
+        const response = await fetch('http://localhost:1337/api/user-routines?populate=*', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData),
+        });
+
+        const result = await response.json();
+
+        if (response.ok) {
+            // Procesar la respuesta de Strapi
+            location.reload();
+        } else {
+            alert('Hubo un error al enviar el formulario');
+            console.error('Error de respuesta:', result);
+        }
+    } catch (error) {
+        console.error('Error al enviar los datos a Strapi:', error);
+    }
+}
+
+async function editUserRoutine(user, userRutine, date, rutineId){
+
+    let rutine = userRutine.rutine
+    console.log(rutineId)
+
+    const formData = {
+        data: {
+            user_app: { connect: [user] }, // Relación con el usuario
+            rutine: { connect: [rutine] },
+            date: date
+        }
+    };
+
+    console.log(formData);
+
+    try {
+        // Enviar los datos a Strapi usando fetch (puedes usar axios también)
+        const response = await fetch(`http://localhost:1337/api/user-routines/${rutineId}?populate=*`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData),
+        });
+
+        const result = await response.json();
+
+        if (response.ok) {
+            // Procesar la respuesta de Strapi
+            location.reload();
+        } else {
+            alert('Hubo un error al enviar el formulario');
+            console.error('Error de respuesta:', result);
+        }
+    } catch (error) {
+        console.error('Error al enviar los datos a Strapi:', error);
+    }
+}
+
+async function deleteUserRoutine(rutineId) {
+    try {
+        // Enviar la solicitud DELETE a Strapi
+        const response = await fetch(`http://localhost:1337/api/user-routines/${rutineId}`, {
+            method: 'DELETE',  // Usamos el método DELETE para eliminar el recurso
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        if (response.ok) {
+            location.reload();  // Recargar la página para reflejar los cambios
+        } else {
+            const result = await response.json();
+            console.error('Error de respuesta:', result);
+        }
+    } catch (error) {
+        console.error('Error al eliminar la rutina:', error);
+    }
+}
