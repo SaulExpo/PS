a947e885b36286ea8a9566e90496b150f64031be
exmosaul <exmosaul@gmail.com>
2025-04-29 16:25:57 +0100

    Cambio en los chats

---
+++ PS-FitPlanner/JavaScript/firebase_config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBTiD4Phk2rKe1sF62c9qDSk1c3jNFgLf4",
+    authDomain: "ps-fitness-app.firebaseapp.com",
+    projectId: "ps-fitness-app",
+    storageBucket: "ps-fitness-app.firebasestorage.app",
+    messagingSenderId: "861934326277",
+    appId: "1:861934326277:web:863ae59d1904e86a00826c",
+    measurementId: "G-3WZD6X5SC2"
+};
