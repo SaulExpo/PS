4e1e03484fb5e4444c4d1197b1e1081e4ea6e8a7
Álvaro <alvarojrr4@gmail.com>
2025-05-13 12:29:16 +0100

    Primeros pasos del Catálogo. Se suben los artículos a la base de datos

---
+++ PS-FitPlanner/Catalogo/DB/uploadSuplemento.js
@@ -0,0 +1,25 @@
+const admin = require('firebase-admin');
+
+
+const productos = require('../../JSON/suplementos.json');
+const serviceAccount = require('./service_account.json');
+
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore();
+
+async function subirProductos() {
+    const grupoRef = db.collection("tienda").doc("suplementos").collection("productos");
+
+    for (const producto of productos) {
+        await grupoRef.add(producto);
+        console.log(`✅ Producto subido: ${producto.name}`);
+    }
+
+    console.log("🎉 Todos los productos han sido subidos.");
+}
+
+subirProductos().catch(console.error);
