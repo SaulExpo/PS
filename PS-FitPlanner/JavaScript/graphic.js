3dddf28a68b4857b849aeb0eb3ddfa990cdbe596
maria <informatica1bachIES@gmail.com>
2025-05-13 15:43:32 +0100

    You can download the graphic

--- PS-FitPlanner/JavaScript/graphic.js
+++ PS-FitPlanner/JavaScript/graphic.js
@@ -1,12 +1,22 @@
 google.charts.load('current', {packages: ['corechart']});
 google.charts.setOnLoadCallback(() => loadData('../JavaScript/calorias.js', 'month'));
 
+let currentChart = null;
+
 function loadData(filename, range = 'month') {
     fetch(filename)
         .then(response => response.json())
         .then(data => {
             if (range === 'week') {
-                data = data.slice(-7);
+                const today = new Date();
+                const last7Days = [];
+                for(let i = 6; i >= 0; i--){
+                    const d = new Date(today);
+                    d.setDate(today.getDate() - i);
+                    const day = d.getDate().toString();
+                    last7Days.push(day);
+                }
+                data = data.filter(item => last7Days.includes(item.dia));
             }
             drawChart(data, filename);
         })
@@ -28,7 +38,7 @@ function drawChart(dataArray, filename) {
     const daysinMonth = new Date(year, now.getMonth()+1, 0).getDate();
     const allDays = [];
     for(let i = 1; i <= daysinMonth; i++) {
-        allDays.push(i.toString().padStart(2, '0'));
+        allDays.push(i.toString());
     }
 
     const dataMap = {};
@@ -84,10 +94,27 @@ function drawChart(dataArray, filename) {
     document.getElementById('monthTitle').textContent = monthName + ' ' + year;
 
 
-    var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
-    chart.draw(data, options);
+    currentChart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
+    currentChart.draw(data, options);
 }
 
 function calcularCalorias(met, peso, minutos) {
     return ((met * peso * 3.5) / 200) * minutos;
+}
+
+
+
+function downloadChart(){
+    if(currentChart){
+        const imageURI = currentChart.getImageURI();
+        const link = document.createElement('a');
+        link.href = imageURI;
+        link.download = 'graphic.png';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    }
+    else{
+        alert("There is not any graphic available to download");
+    }
 }
