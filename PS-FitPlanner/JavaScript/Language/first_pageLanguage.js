b64c7f83100a1a1203a2bde10df052b859ac8c50
Saul <exmosaul@gmail.com>
2025-05-14 18:15:41 +0100

    Cambio de idioma de calendario y first Page

---
+++ PS-FitPlanner/JavaScript/Language/first_pageLanguage.js
@@ -0,0 +1,29 @@
+export async function first_pageLanguage() {
+    let language = localStorage.getItem("language");
+    let json_language = language === "english"
+        ? "../JSON/English/first_pageEnglish.json"
+        : "../JSON/Español/first_pageEspañol.json"
+    fetch(json_language)
+        .then(function (res) {
+            return res.json();
+        })
+        .then(function (json) {
+            console.log(json);
+            document.querySelectorAll(".info")[0].textContent = json.first_page.today;
+            document.querySelectorAll(".info")[1].textContent = json.first_page.calendar;
+            document.querySelectorAll("#picture")[0].src = json.first_page.today_img;
+            document.querySelectorAll("#picture")[1].src = json.first_page.calendar_img;
+            document.querySelectorAll("#picture")[1].addEventListener("click", () => {
+                location.replace("../Pages/calendar.html");
+            });
+
+            document.querySelectorAll("#picture")[2].src = json.first_page.recommendations_image;
+            document.querySelectorAll("#picture")[2].alt = "Recommendations";
+            document.getElementById("ejercicios").textContent = json.first_page.recommendations_title
+            document.getElementById("calendar").textContent = json.first_page.calendar_title
+            document.querySelectorAll(".title")[0].textContent = json.first_page.today_title
+        })
+        .catch(function (err) {
+            console.error("Error cargando JSON:", err);
+        });
+}
